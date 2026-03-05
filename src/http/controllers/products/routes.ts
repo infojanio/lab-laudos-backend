2e1d321796853403f5d8d9b3b685094fc34b6a62
info.janio@gmail.com <info.janio@gmail.com>
2026-03-05 08:31:06 -0300

    estrutura de pastas

--- src/http/controllers/products/routes.ts
+++ src/http/controllers/products/routes.ts
@@ -4,7 +4,7 @@ import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 import { create } from "./create";
 import { listProducts } from "./listProducts";
 import { fetchProductsBySubCategory } from "./fetch-products-by-subcategory";
-import { fetchProductsByCashback } from "./fetch-products-by-cashback";
+
 import { fetchProductsByQuantity } from "./fetch-products-by-quantity";
 import { updateProduct } from "./update-product";
 import { getStock, updateStock } from "./get-stock";
@@ -12,7 +12,7 @@ import { getProduct } from "./get-product";
 import { searchProducts } from "./search-products";
 import { listProductsActive } from "./listProductsActive";
 import { listProductsByStoreController } from "./list-products-by-store";
-import { listProductsByStoreWithDiscountController } from "./list-products-by-store-with-discount";
+
 import { lowStock } from "./low-stock";
 
 export async function productsRoutes(app: FastifyInstance) {
@@ -20,7 +20,7 @@ export async function productsRoutes(app: FastifyInstance) {
 
   // Rotas de consulta
   app.get("/products/quantity", fetchProductsByQuantity);
-  app.get("/products/cashback", fetchProductsByCashback);
+
   app.get("/products/active", listProductsActive);
   app.get("/products/subcategory", fetchProductsBySubCategory);
   app.get("/products", listProducts);
@@ -28,11 +28,6 @@ export async function productsRoutes(app: FastifyInstance) {
   // Listar produtos da loja
   app.get("/stores/:storeId/products", listProductsByStoreController);
 
-  // Produtos da loja (COM desconto aplicado)
-  app.get(
-    "/stores/:storeId/products/with-discount",
-    listProductsByStoreWithDiscountController,
-  );
 
   // Rota para detalhes do produto
   app.get("/products/search", searchProducts);
