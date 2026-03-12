3621ca257ad4463007dcc9ea7d856714f6a1a6d1
Janio Florêncio <info.janio@gmail.com>
2026-03-12 20:53:09 -0300

    módulo clients

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
   JWT_SECRET: z.string(),
-  PORT: z.coerce.number().default(3333), // porta na railway: 33433 , porta local: 3333
+  PORT: z.coerce.number().default(33433), // porta na railway: 33433 , porta local: 3333
   DATABASE_URL: z.string().url(),
 });
 
